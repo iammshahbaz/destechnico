dcfc4aa6a566f0219eeaa882b1fba5f6c99b5716
iammshahbaz <shahbaz.ahamed1995@gmail.com>
2024-09-02 13:56:32 +0530

    port

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ app.use((req, res) => {
     res.status(404).json({ message: 'Resource not found.' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
