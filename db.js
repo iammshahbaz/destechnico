f17d59c835977c8409d62f0d6df62840e93962aa
iammshahbaz <shahbaz.ahamed1995@gmail.com>
2024-09-09 19:09:57 +0530

    deploy

--- db.js
+++ db.js
@@ -1,3 +1,27 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
+
+// pool.on('connect', () => {
+//     console.log('Connected to the PostgreSQL database');
+// });
+
+// pool.on('error', (err) => {
+//     console.error('Error connecting to the PostgreSQL database:', err);
+// });
+
+
+
+
+// // module.exports = pool;
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -7,6 +31,9 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false 
+    }
 });
 
 pool.on('connect', () => {
@@ -17,7 +44,5 @@ pool.on('error', (err) => {
     console.error('Error connecting to the PostgreSQL database:', err);
 });
 
-
-
-
 module.exports = pool;
+
